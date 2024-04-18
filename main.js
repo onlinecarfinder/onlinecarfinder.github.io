d5be018214c9c790750fb39c7763e85a8acaa140
Online Car Finder <157360280+onlinecarfinder@users.noreply.github.com>
2024-04-18 14:47:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Function to fetch data from the JSON file
     async function fetchData() {
         try {
-            const response = await fetch('fewdatas.json'); // Fetch the JSON file
+            const response = await fetch('fewfewdatas.json'); // Fetch the JSON file
             if (!response.ok) {
                 throw new Error('Failed to fetch data');
             }
